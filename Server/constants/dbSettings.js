d80ef93e2d99f45a250ecfbee00142a4b0217308
unknown <ngochoang8709@gmail.com>
2023-05-12 22:05:41 +0700

    New

--- Server/constants/dbSettings.js
+++ Server/constants/dbSettings.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CONNECTION_STRING: 'mongodb+srv://ngochoang8709:<nnhntbn1331>@my-db.tsai8ze.mongodb.net/?retryWrites=true&w=majority',
+  CONNECTION_STRING: 'mongodb+srv://my-mongodb:Nnh1206@my-db.tsai8ze.mongodb.net/?retryWrites=true&w=majority',
   DATABASE_NAME: 'Object',
 };
